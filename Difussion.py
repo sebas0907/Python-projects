322e0bfb9f38e8de2c77fd1083e25e4d8aa448f4
sebas0907 <39376403+sebas0907@users.noreply.github.com>
2020-07-06 17:01:33 +0200

    update

--- Difussion.py
+++ Difussion.py
@@ -1,30 +1,30 @@
 from math import sin, exp, pi, cos, sqrt
 import matplotlib.pyplot as plt
 import numpy as N
-#This is my very own implementation of the Crank-Nicolson method for the 1D heat equation
-L = 1.0
-u = 0.2
-alpha = 4.0
-dt = 0.001
-dx = 0.01
-K = alpha*dt/dx**2
-n = (int)(L/dx)
-q = (int)(u/dt)
-x = N.linspace(0, L, n+1) #for some reason a stupid list can be plotted instead of an array.
+#This is an explicit implementation of the Crank-Nicolson method for the heat equation in the case of a one-dimensional rod.
+L = 1.0 #Lenght of the rod
+u = 0.2 #final time
+alpha = 4.0 'diffusion coefficient
+dt = 0.001 #time interval
+dx = 0.01 #grid interspacing size
+K = alpha*dt/dx**2 
+n = (int)(L/dx) #number of grid points
+q = (int)(u/dt) #number of time intervals
+x = N.linspace(0, L, n+1) #definition of axes
 t = N.linspace(0, u, q+1) 
-A = N.zeros((n+1,n+1))
+A = N.zeros((n+1,n+1)) #initial variables
 B = N.zeros((n+1,n+1))
 b = N.zeros(n+1)
-T = N.zeros(n+1)
-Tn = N.zeros(n+1)
+T = N.zeros(n+1) #temperature at previous step
+Tn = N.zeros(n+1) #Temperature at new step
 def f(x):
     return sin(pi*x)
 for i in range(1,n):
         T[i] = f(x[i]) #initial condition
 for i in range(1,n):
     A[i,i-1] = -K/2
-    A[i,i+1] = -K/2   #A and B are both tri-diagonal matrices but the
-    A[i,i] = 1 + K    #initial conditions must be forced for consistency
+    A[i,i+1] = -K/2   #A and B are both tri-diagonal matrices and the
+    A[i,i] = 1 + K    #initial conditions must be forced here for consistency
     A[0,0] = A[n,n] = 1
 for i in range(1,n):
     B[i,i-1] = K/2
@@ -37,7 +37,7 @@ for i in range(0,q): #temporal evolution of the temperature for each T(x)
     for j in range(1,n):
         b[j] = T[j]
     b[0] = b[n] = 0
-    Tn[:] = N.linalg.solve(A,N.dot(B,b)) #Solve equation A*x[n+1] = B*x[n] for x[n+1]
+    Tn[:] = N.linalg.solve(A,N.dot(B,b)) #Solve equation A*T[n+1] = B*T[n] for T[n+1]
     T[:] = Tn
     plt.xlabel('Distance')
     plt.ylabel('Temperature')
@@ -45,7 +45,7 @@ for i in range(0,q): #temporal evolution of the temperature for each T(x)
     plt.axis([0,L,-0.1,1.0])
     plt.grid()
     plt.plot(x,abs(T))
-    plt.pause(0.0001)
+    plt.pause(0.0001) #the solution is shown as an animation 
 plt.show()
 
 
