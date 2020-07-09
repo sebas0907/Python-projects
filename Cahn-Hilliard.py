9ab9bf3c89e8d4fea2807722909d897dca35fb9d
sebas0907 <39376403+sebas0907@users.noreply.github.com>
2020-07-09 17:23:36 +0200

    updated

--- Cahn-Hilliard.py
+++ Cahn-Hilliard.py
@@ -1,31 +1,53 @@
-from math import sin, exp, pi, cos, sqrt, tanh
-import matplotlib.pyplot as plt
 import numpy as N
+import matplotlib.pyplot as plt
 import random
-L = 1.0
-u = 0.5
-h = 1.0
-dt = 0.01 #timestep size
-M = 1.0 #mobility
-dx = dy = h #grid spacing size
-A = 1.0 
-kappa = 0.5 #gradient coefficient
-nx = 64#(int)(L/dx) #number of grid points 
-ny = nx
-q = 20000#(int)(u/dt)
-c0 = 0.4 #Average concentration value
-U = N.zeros((nx,ny)) #concentration at next step
-f0 = N.zeros((nx,ny))#variation of the chemical energy
-c = N.zeros((nx,ny)) #initial concentration
+T = 100 #final time
+dx = 1.0
+dy = dx
+dt = 0.01
+n=64
+nx = ny = n
+kappa = 0.5 #grad-coefficient
+c0 = 0.4 #average cocentration
+M = 4.0 #mobility
+A = N.zeros((n,n))
+L = N.zeros((n,n))
+H = N.zeros((n,n)) #clear variables
+f = N.zeros((n,n))
+F = N.zeros((n,n))
 
-c = c0 + 0.02*(0.5-N.random.rand(nx,ny)) #initial condition 
-f0 = 2*c*(1-c)**2-2*c**2*(1-c)#c**3 - c #Chemical energy double-well potential
-mfig = [1,20,50,100] #output figures at these timesteps
-fig = plt.figure()
-num = 0
-for m in range(1,101):
-    for i in range(nx):
-        for j in range(ny):
+A = c0 + 0.02*(0.5-N.random.rand(n,n)) #random noise
+#boundary conditions
+A[0,:] = A[n-1,:]
+A[:,0] = A[:,n-1] 
+f= A**3-A#2*A*(1-A)**2-2*A**2*(1-A) #variation of chemical energy
+g= A**2-1 #second derivative of chemical energy
+g0=c0**2-1 #second derivative evaluated at the average 
+#fig = plt.figure()
+#mfig = [1,100,200,300]
+def laplace(Z,d_x,d_y):
+    for i in range(n):
+        for j in range(n):
+            jp=j+1
+            jm=j-1
+            ip=i+1
+            im=i-1
+            if ip > nx-1:  
+                ip = ip - nx
+            if im < 0:
+                im = im + nx
+            if jp > ny-1:
+                jp = jp - ny
+            if jm < 0:
+                jm = jm + ny
+            uxx = (Z[ip,j] - 2*Z[i,j] + Z[im,j]) / dx**2
+            uyy = (Z[i,jp] - 2*Z[i,j] + Z[i,jm]) / dy**2
+            L[i,j] =  (uxx + uyy)
+    return L
+print('wait please...')
+def del4(c,d_x,d_y):
+    for i in range(n):
+        for j in range(n):
             jp=j+1
             jm=j-1
             ip=i+1
@@ -34,36 +56,48 @@ for m in range(1,101):
             jmm=j-2
             ipp=i+2
             imm=i-2
-            if ip > nx-1:  # periodic boundary condition
-                ip = ip - nx
+            if ip > n-1:  
+                ip = ip - n
             if im < 0:
-                im = im + nx
-            if jp > ny-1:
-                jp = jp - ny
+                im = im + n
+            if jp > n-1:
+                jp = jp - n
             if jm < 0:
-                jm = jm + ny
-            if ipp > nx-1: 
-                ipp = ipp - nx
+                jm = jm + n
+            if ipp > n-1: 
+                ipp = ipp - n
             if imm < 0:
-                imm = imm + nx
-            if jpp > ny-1:
-                jpp = jpp - ny
+                imm = imm + n
+            if jpp > n-1:
+                jpp = jpp - n
             if jmm < 0:
-                jmm = jmm + ny
-                Uxxxx = (c[imm,j]-4*c[im,j]+6*c[i,j]-4*c[ip,j]+c[ipp,j])/dx**4 
-                Uyyyy = (c[i,jmm]-4*c[i,jm]+6*c[i,j]-4*c[i,jp]+c[i,jpp])/dy**4
-                Uxxyy = (c[ip,jp]-2*c[i,jp]+c[im,jp]-2*c[ip,j]+4*c[i,j]-2*c[im,j]+c[ip,jm]-2*c[i,jm]+c[im,jm])/(dy**2*dx**2) #centered finite differences
-                fxx = (f0[ip,j] - 2*f0[i,j] + f0[im,j]) / dx**2
-                fyy = (f0[i,jp] - 2*f0[i,j] + f0[i,jm]) / dy**2   
-                H = Uxxxx+Uyyyy+2*Uxxyy #second Laplacian of the concentration
-                F = fxx + fyy #Laplacian of the derivative of the Chemical energy
-                U = c + M*dt*(F - kappa*H) #Cahn-Hilliard equation
-    c = U
-    if m in mfig:
-        num += 1
-        ax = fig.add_subplot(220 + num) #2x2 grid, subplot: 0 + num
-        im = ax.imshow(U,interpolation='lanczos',cmap='bwr')
-fig.subplots_adjust(right=0.8)
-cbar_ax = fig.add_axes([0.9, 0.15, 0.03, 0.7])
-fig.colorbar(im,cax=cbar_ax)
+                jmm = jmm + n
+            uxxxx = (c[imm,j]-4*c[im,j]+6*c[i,j]-4*c[ipp,j]+c[ipp,j])/dx**4 
+            uyyyy = (c[i,jmm]-4*c[i,jm]+6*c[i,j]-4*c[i,jp]+c[i,jpp])/dy**4
+            uxxyy = (c[ip,jp]-2*c[i,jp]+c[im,jp]-2*c[ip,j]+4*c[i,j]-2*c[im,j]+c[ip,jm]-2*c[i,jm]+c[im,jm])/(dy**2*dx**2)
+            H[i,j] = uxxxx +uyyyy +2*uxxyy
+    return H
+num = 0
+#Animation
+for m in range(T):
+    plt.cla()
+    F = laplace(A,dx,dy)
+    D = del4(A,dx,dy)
+    An = A + M*dt*(g0*F - kappa*D) #Cahn-Hilliard equation
+    A = An
+##    if m % (T/4) == 0: #print four figures for equal time intervals
+##        num += 1
+##        ax = fig.add_subplot(220 + num) #2x2 grid, subplot: 0 + num
+##        im = ax.imshow(An,interpolation='lanczos',cmap='bwr')
+##fig.subplots_adjust(right=0.8)
+##cbar_ax = fig.add_axes([0.9, 0.15, 0.03, 0.7])
+##fig.colorbar(im,cax=cbar_ax)
+##plt.show()
+    plt.title('Spinoidal decomposition')
+    im = plt.imshow(An,interpolation='lanczos',cmap='bwr')
+    plt.pause(0.00001)
+cbar = plt.colorbar()
+cbar.set_label('Concentration')
 plt.show()
+
+
